d11788757b06e022cf9aa5b07e729bcdc91e885e
David Senk <david@ything.net>
2025-06-15 17:01:44 -0400

    add base args expected

---
+++ src/args.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about = None, long_about = None)]
+pub struct Args {
+    #[arg(default_value_t = false)]
+    /// SHA2-256
+    #[arg(long)]
+    pub sha256: bool,
+    /// SHA3-256
+    #[arg(long)]
+    pub sha3_256: bool,
+    /// SHA1
+    #[arg(long)]
+    pub sha1: bool,
+    /// CRC_32_ISO_HDLC
+    #[arg(long)]
+    pub crc32_iso: bool,
+    /// CRC_32_CKSUM
+    #[arg(long)]
+    pub crc32_posix: bool,
+    /// CRC_32_XFER
+    #[arg(long)]
+    pub crc32_xfer: bool,
+    /// Blake2b256
+    #[arg(long)]
+    pub blake256: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+use hashstream::Args;
+
+pub struct Hashes {
+    hashes: Args,
+}
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
